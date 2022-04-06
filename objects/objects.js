e387130c3be03d62af115b177261b85073537ff2
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-04-07 00:00:59 +0300

    second task

--- objects/objects.js
+++ objects/objects.js
@@ -15,3 +15,26 @@ user.name = 'Pete';
 console.log(user);
 delete user.name;
 console.log(user);
+
+// **************************************** Задача 2 ********************************************************
+console.log('task2');
+// Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
+
+// Должно работать так:
+
+let schedule = {};
+
+console.log(isEmpty(schedule)); // true
+
+schedule["8:30"] = "get up";
+
+console.log(isEmpty(schedule)); // false
+
+// **************************************** Решение 2 ********************************************************
+
+function isEmpty(obj) {
+    for (key in obj) {
+        return false;
+    }
+    return true;
+}
