75fb29209a509a7528a7d121d275179f26383c37
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-04-07 00:18:11 +0300

    4 task

--- objects/objects.js
+++ objects/objects.js
@@ -60,3 +60,30 @@ console.log(out2);
 
 // **************************************** Задача 4 ********************************************************
 console.log('task4');
+// Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
+
+// Например:
+
+// до вызова функции
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// после вызова функции
+console.log(menu);
+// menu = {
+//     width: 400,
+//     height: 600,
+//     title: "My menu"
+// };
+
+function multiplyNumeric(menu) {
+    for (key in menu) {
+        if (typeof menu[key] == 'number') menu[key] *= 2;
+
+    }
+}
