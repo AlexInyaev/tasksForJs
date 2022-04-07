b112331ae24b311d6c52f0cafea8f90252cd3908
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-04-08 00:35:35 +0300

    первая задача

---
+++ objects/методы обекта/js.js
@@ -0,0 +1,10 @@
+let calculator = {
+    // ... ваш код ...
+    read: function () { this.firstNumber = prompt('first number', 22); this.secondNumber = prompt('second number', 15) },
+    sum: function () { return +this.firstNumber + +this.secondNumber },
+    mul: function () { return this.firstNumber * this.secondNumber }
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
