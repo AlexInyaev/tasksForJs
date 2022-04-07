54f0a6387bad6acb45f71e470a938bbedbc74e04
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-04-08 01:01:25 +0300

    решил вторую задачу

--- objects/методы обекта/js.js
+++ objects/методы обекта/js.js
@@ -6,7 +6,49 @@ let calculator = {
     mul: function () { return this.firstNumber * this.secondNumber }
 };
 
-calculator.read();
-alert(calculator.sum());
-alert(calculator.mul());
+// calculator.read();
+// alert(calculator.sum());       закомментировал чтобы не мешали 
+// alert(calculator.mul());
 
+// **************************************** Задача 2 ********************************************************
+// Это ladder (лестница) – объект, который позволяет подниматься вверх и спускаться:
+
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+    },
+    down() {
+        this.step--;
+    },
+    showStep: function () { // показывает текущую ступеньку
+        // alert(this.step);
+    }
+};
+// Теперь, если нам нужно сделать несколько последовательных вызовов, мы можем выполнить это так:
+
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+// Измените код методов up, down и showStep таким образом, чтобы их вызов можно было сделать по цепочке, например так:
+
+// ladder.up().up().down().showStep(); // 1
+// **************************************** Задача 2 решение ********************************************************
+
+let ladder2 = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep: function () { // показывает текущую ступеньку
+        alert(this.step);
+    }
+};
+ladder2.up().up().down().showStep()// 1
+console.log(ladder2.step);
