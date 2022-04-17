fa7433157c67ac91dd6a5efc8b3bd04606fbda5c
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-04-18 02:10:50 +0300

    add task

--- Map and Set/Map.js
+++ Map and Set/Map.js
@@ -13,3 +13,44 @@ let values = ["Hare", "Krishna", "Hare", "Krishna",
 ];
 
 console.log(unique(values)); // Hare,Krishna,:-O
+
+
+// **************************************** Задача 2 ********************************************************
+console.log('task - 2');
+// Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
+
+function aclean(arr) {
+    let map = new Map();
+    let out = [];
+
+
+    arr.forEach(value => {
+        map.set(Array.from(value.toLowerCase()).sort().join(''), value)
+
+    });
+    for (let item of map.values()) {
+        out.push(item)
+    }
+
+    return out;
+
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert(aclean(arr)); // "nap,teachers,ear" или "PAN,cheaters,era"
+// Из каждой группы анаграмм должно остаться только одно слово, не важно какое.
+
+//Решение из книги
+
+// function aclean(arr) {
+//     let map = new Map();
+
+//     for (let word of arr) {
+//         // разбиваем слово на буквы, сортируем и объединяем снова в строку
+//         let sorted = word.toLowerCase().split("").sort().join(""); // (*)
+//         map.set(sorted, word);
+//     }
+
+//     return Array.from(map.values());
+// }
