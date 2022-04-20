7fe4eaed327e4d820d6bf44de016ca285a56bb28
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-04-21 01:56:33 +0300

    add task

--- Date/dateObj.js
+++ Date/dateObj.js
@@ -1,5 +1,6 @@
 
 // task_1();
+task_2()
 
 function task_1() {
     // Создайте объект Date для даты: 20 февраля 2012 года, 3 часа 12 минут.Временная зона – местная.
@@ -12,4 +13,50 @@ function task_1() {
     let d = new Date(2012, 1, 20, 3, 12);  // 2 вариант
     alert(d);
 
+}
+
+function task_2() {
+    // Напишите функцию getWeekDay(date), показывающую день недели в коротком формате: «ПН», «ВТ», «СР», «ЧТ», «ПТ», «СБ», «ВС».
+
+
+    let date = new Date(2012, 0, 3);  // 3 января 2012 года
+    alert(getWeekDay(date));        // нужно вывести "ВТ"
+
+    function getWeekDay(date) {
+        let dayWeek = '';
+        switch (date.getDay()) {
+            case 0:
+                dayWeek = 'вс';
+                break;
+            case 1:
+                dayWeek = 'пн';
+                break;
+            case 2:
+                dayWeek = 'вт';
+                break;
+            case 3:
+                dayWeek = 'ср';
+                break;
+            case 4:
+                dayWeek = 'чт';
+                break;
+            case 5:
+                dayWeek = 'пт';
+                break;
+            case 6:
+                dayWeek = 'сб';
+                break;
+
+        }
+        return dayWeek;
+    }
+
+    function getWeekDay2(date) {
+        let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+
+        return days[date.getDay()];
+    }
+
+    let date2 = new Date(2014, 0, 3); // 3 января 2014 года
+    alert(getWeekDay2(date)); // ПТ
 }
