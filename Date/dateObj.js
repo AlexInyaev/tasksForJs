a3f3389f1b7acc9c4b6d3e530d8a3b1d7afe08d0
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-04-21 02:13:36 +0300

    add task

--- Date/dateObj.js
+++ Date/dateObj.js
@@ -1,6 +1,7 @@
 
 // task_1();
-task_2()
+// task_2()
+task_3()
 
 function task_1() {
     // Создайте объект Date для даты: 20 февраля 2012 года, 3 часа 12 минут.Временная зона – местная.
@@ -60,4 +61,21 @@ function task_2() {
 
     let date2 = new Date(2014, 0, 3); // 3 января 2014 года
     alert(getWeekDay2(date)); // ПТ
+}
+
+function task_3() {
+    // Создайте функцию getDateAgo(date, days), возвращающую число, которое было days дней назад от даты date.
+    // К примеру, если сегодня двадцатое число, то getDateAgo(new Date(), 1) вернёт девятнадцатое и getDateAgo(new Date(), 2) – восемнадцатое.
+    // Функция должна надёжно работать при значении days=365 и больших значениях:
+
+    let date = new Date(2015, 0, 2);
+
+    alert(getDateAgo(date, 1)); // 1, (1 Jan 2015)
+    alert(getDateAgo(date, 2)); // 31, (31 Dec 2014)
+    alert(getDateAgo(date, 365)); // 2, (2 Jan 2014)
+    // P.S. Функция не должна изменять переданный ей объект date.
+
+    function getDateAgo(date, dayAgo) {
+        return new Date(date - (dayAgo * 24 * 3600 * 1000)).getDate();
+    }
 }
