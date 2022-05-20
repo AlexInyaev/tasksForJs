20156b410dac6e3838fac617a8d5c31dec4b285b
AlexInyaev <46852628+AlexInyaev@users.noreply.github.com>
2022-05-21 00:21:52 +0300

    .

--- NFE/recurs.js
+++ NFE/recurs.js
@@ -17,15 +17,37 @@
 
     let counter = makeCounter();
 
-    alert(counter()); // 0
-    alert(counter()); // 1
+    // alert(counter()); // 0
+    // alert(counter()); // 1
 
     counter.set(10); // установить новое значение счётчика
 
-    alert(counter()); // 10
+    // alert(counter()); // 10
 
     counter.decrease(); // уменьшить значение счётчика на 1
 
-    alert(counter()); // 10 (вместо 11)
+    // alert(counter()); // 10 (вместо 11)
+
+}
+{
+
+    function sum(a) {
+        let count = a;
+        function f(x) {
+            count += x;
+            return f;
+        }
+        f.toString = function () {
+            return count;
+        }
+        return f;
+    }
+    console.log(sum(1)(2) == 3);
+    sum(1)(2) == 3; // 1 + 2
+    sum(1)(2)(3) == 6; // 1 + 2 + 3
+    sum(5)(-1)(2) == 6
+    sum(6)(-1)(-2)(-3) == 0
+    sum(0)(1)(2)(3)(4)(5) == 15
+
 
 }
